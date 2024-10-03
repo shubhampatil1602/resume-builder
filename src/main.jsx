59ea9fc5e575fdfcfa21df74a790cbd5edce3f79
Shubham Patil <shubhamspatilnbr@gmail.com>
2024-10-04 03:57:20 +0530

    fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ResumeProvider } from './contexts/resumeContext.jsx';
+import { ResumeProvider } from './contexts/ResumeContext.jsx';
 import Write from './pages/Write.jsx';
 import ResumeContents from './pages/ResumeContents.jsx';
 
